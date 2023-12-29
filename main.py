1833ea309323442d1c0c33c25e172399d00cc18c
Panos Dask <panosdaskalopoulos259@gmail.com>
2023-12-29 16:20:40 +0200

    Added file printing to report solution

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 #from TSP_Model import Model
 from Solver import *
+FILE_NAME = 'test.txt'
 
 m = Model()
 m.BuildModel()
 s = Solver(m)
 sol = s.solve()
-s.ReportSolutionToFile(sol,"test.txt")
+s.ReportSolutionToFile(sol, FILE_NAME)
+with open(FILE_NAME, 'r') as f:
+    print("\n"+f.read())
