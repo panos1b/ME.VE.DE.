b2e1f063dda26202a55309933c59ca3fe91f5e40
Panos Dask <panosdaskalopoulos259@gmail.com>
2024-01-06 16:50:36 +0200

    No arcs to depo

--- SolutionDrawer.py
+++ SolutionDrawer.py
@@ -16,7 +16,7 @@ class SolDrawer:
     def drawPoints(nodes:list):
         x = []
         y = []
-        for i in range(len(nodes)):
+        for i in range(0, len(nodes)):
             n = nodes[i]
             x.append(n.x)
             y.append(n.y)
@@ -28,7 +28,7 @@ class SolDrawer:
         if sol is not None:
             for r in range(0, len(sol.routes)):
                 rt = sol.routes[r]
-                for i in range(0, len(rt.sequenceOfNodes) - 1):
+                for i in range(1, len(rt.sequenceOfNodes) - 2):
                     c0 = rt.sequenceOfNodes[i]
                     c1 = rt.sequenceOfNodes[i + 1]
                     plt.plot([c0.x, c1.x], [c0.y, c1.y], c=cmap(r))
